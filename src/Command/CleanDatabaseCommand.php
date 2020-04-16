6e3150437aa9e57379c99c585fc7fc778fb4896f
symac <web@geobib.fr>
2020-04-16 07:24:24 +0200

    Improve clean-db command to remove anagrams from DB if needed

--- src/Command/CleanDatabaseCommand.php
+++ src/Command/CleanDatabaseCommand.php
@@ -3,6 +3,8 @@
 namespace App\Command;
 
 use App\Entity\Forename;
+use App\Entity\Result;
+use App\Entity\ResultStep;
 use App\Repository\ForenameRepository;
 use App\Service\SlugGenerator;
 use Doctrine\ORM\EntityManagerInterface;
@@ -10,6 +12,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Finder\Finder;
 
@@ -28,15 +31,15 @@ class CleanDatabaseCommand extends Command
     protected function configure()
     {
         $this
-            ->setDescription("Remove from DB anything that won't get used")
-        ;
+            ->setDescription("Remove from DB anything that won't get used");
     }
 
-    private function runSql($sql) {
+    private function runSql($sql)
+    {
         $conn = $this->em->getConnection();
         $stmt = $conn->prepare($sql);
         $stmt->execute();
-        return  $stmt->rowCount();
+        return $stmt->rowCount();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -64,10 +67,25 @@ class CleanDatabaseCommand extends Command
         $count = $this->runSql('DELETE FROM `surname` where label like \'"%\'');
         $io->writeln(" > $count deletions\n");
 
+        $helper = $this->getHelper('question');
+        $question = new ConfirmationQuestion("Do you also want to remove all anagrams already found [y or n]", false);
+        $answer = $helper->ask($input, $output, $question);
+        if ($answer == true) {
+            $resultSteps = $this->em->getRepository(ResultStep::class)->findAll();
+            foreach ($resultSteps as $resultStep) {
+                $this->em->remove($resultStep);
+            }
+            $this->em->flush();
 
-//;
-//;
-
+            $count = 0;
+            $results = $this->em->getRepository(Result::class)->findAll();
+            foreach ($results as $result) {
+                $this->em->remove($result);
+                $count++;
+            }
+            $this->em->flush();
+            $io->writeln("> $count deletions");
+        }
         exit;
 
     }
