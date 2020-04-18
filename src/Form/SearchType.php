3a254034e042620544c037184fe172197421e79b
symac <web@geobib.fr>
2020-04-18 10:39:25 +0200

    Make things more clear

--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -14,7 +14,7 @@ class SearchType extends AbstractType
     {
         $builder
             ->add('search_name', TextType::class, ["label" => ""])
-            ->add("submit", SubmitType::class, ["label" => "➡ Find anagrams for this name"])
+            ->add("submit", SubmitType::class, ["label" => "Get anagrams for this full name"])
         ;
     }
 
