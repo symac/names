19ae13ac1793a1546d26f095c715772b156c9530
symac <web@geobib.fr>
2021-06-22 14:31:06 +0200

    Mise à jour version 5.1

--- src/Repository/ForenameRepository.php
+++ src/Repository/ForenameRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Forename;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Forename|null find($id, $lockMode = null, $lockVersion = null)
--- src/Repository/SurnameRepository.php
+++ src/Repository/SurnameRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Surname;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Surname|null find($id, $lockMode = null, $lockVersion = null)
